c345edd999a60c4e4f4ce34a3aad35a543e5e27e
Sahil Gupta <drxsahilgupta@gmail.com>
2025-11-30 15:58:17 +0530

    Initial upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll
+document.querySelectorAll("a[href^='#']").forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
